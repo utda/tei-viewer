d83f431054ed1b703fbf650f43e953232cb15151
Satoru <you@example.com>
2024-01-25 13:00:41 +0900

    feat: add mirador

--- src/app/components/ceteicean.tsx
+++ src/app/components/ceteicean.tsx
@@ -5,7 +5,6 @@ import { useEffect, Suspense } from "react";
 
 function CeteiceanContent() {
   const searchParams = useSearchParams();
-
   useEffect(() => {
     const teiFileUrl =
       searchParams.get("u") ||
@@ -31,10 +30,24 @@ function CeteiceanContent() {
   return <div id="TEI"></div>;
 }
 
+//  style={{ height: "600px" }}
 export default function Ceteicean() {
+  const manifest = "https://www.dl.ndl.go.jp/api/iiif/3437686/manifest.json";
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <CeteiceanContent />
+      <div className="grid grid-cols-2 gap-4" style={{ height: "100vh" }}>
+        <div style={{ height: "100%", overflow: "auto" }}>
+          <CeteiceanContent />
+        </div>
+
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://mirador.cultural.jp/?manifest=${manifest}`}
+          title="Mirador Viewer"
+          style={{ border: "none" }}
+        ></iframe>
+      </div>
     </Suspense>
   );
 }
